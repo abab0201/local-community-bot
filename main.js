ad57b156b007fe03819bef2d80a1f0baf98c2c99
abab0201 <abab020103@gmail.com>
2026-02-26 17:38:51 +0900

    docs: add logic for Discord to LINE forwarding

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
 // Local Community Discord-LINE Bridge
+// 具体的処理の実装案
+
+var LINE_NOTIFY_TOKEN = "YOUR_LINE_TOKEN"; // あとで自分のトークンを入れる場所
+
 function doPost(e) {
   var json = JSON.parse(e.postData.contents);
-  console.log("Success: " + JSON.stringify(json));
+  
+  // Discordからのメッセージ内容を取得
+  var userName = json.username;
+  var message = json.content;
+  
+  if (message) {
+    sendToLine(userName + ": " + message);
+  }
+}
+
+function sendToLine(text) {
+  // LINE Notifyなどを利用してメッセージを飛ばすロジック
+  console.log("LINEへ送信中: " + text);
+  // 実際にはここに UrlFetchApp.fetch() などを記述します
 }
